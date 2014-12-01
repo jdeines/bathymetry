cba765e80133636b5bd17faa5d92a01cc57544ac
Jill Deines <jillian.deines@gmail.com>
2014-12-01 16:45:08 -0500

    loading and visualizing data

--- data_exploration.R
+++ data_exploration.R
@@ -1,10 +1,28 @@
-library(rgdal)
-
+# Setup ------------------------------------------------------------------------
 setwd("S:/Projects/2013/Higgins_Lake/Higgins_Bath/KrigingProject")
 
+# packages needed
+library(rgdal)
+library(RColorBrewer)
+library(classInt)
+
+
+# Load Data and Plot------------------------------------------------------------
 points <- readOGR("../GIS","Kayak_UM_MSU_2014")
 
-#Hello group mates!!
+# histogram of bottom elevation
+hist(points$Bot_Ele, main='Histogram of Bottom Elevation (m ASL)', 
+  xlab = 'Bottom Elevation (m ASL)')
 
-#hahaha
+# make lake depth (m)
+points$lakedepth <- 351.733 - points$Bot_Ele
+
+# map
+par(mar=c(.5,.5,.5,.5))
+nclasses
+class <- classIntervals(points$lakedepth, 
+
+plot(points, col='blue')
+spplot(points, 'lakedepth', col.regions=terrain.colors(5))
+
 
