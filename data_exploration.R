15fd35a9a9c78ba19a8bfe71b276b5271ce96434
Xiao Liu <liuxia32@GLG-GEOWHATEVER.cns.msu.edu>
2014-12-03 14:53:30 -0500

    add shorelinepoints & explore variogram

--- data_exploration.R
+++ data_exploration.R
@@ -4,17 +4,34 @@ setwd("S:/Projects/2013/Higgins_Lake/Higgins_Bath/KrigingProject")
 # packages needed
 library(rgdal)
 library(grid)
+library(gstat)
+library(maptools)
+
 
 
 # Load Data and Plot------------------------------------------------------------
 points <- readOGR("../GIS","Kayak_UM_MSU_2014")
 
-# histogram of bottom elevation
-hist(points$Bot_Ele, main='Histogram of Bottom Elevation (m ASL)', 
-  xlab = 'Bottom Elevation (m ASL)')
-
 # make lake depth (m) using elevation of shoreline
 points$lakedepth <- 351.733 - points$Bot_Ele
+
+# histogram of bottom elevation
+hist(points$lakedepth, main='Histogram of Lake Depth (m)', 
+  xlab = 'Lake Depth (m)')
+
+# look at qq plots for untransformed and transformed data
+par(mfrow=c(1,3))
+qqnorm(points$lakedepth, main = 'qq-plot-untransformed')
+qqline(points$lakedepth)
+
+# sqrt root
+qqnorm(sqrt(points$lakedepth), main = 'qq-plot-sqrt')
+qqline(sqrt(points$lakedepth))
+# log
+qqnorm(log(points$lakedepth), main = 'qq-plot-log')
+qqline(log(points$lakedepth))
+
+
 
 # make a map
 
@@ -28,3 +45,38 @@ spplot(points, 'lakedepth', col.regions=rev(lakePal(200)), colorkey=T,
 grid.text('Lake Depth (m)',x=unit(0.965, "npc"),y=unit(0.5, 'npc'), rot=-90)
 
 #dev.off()
+
+
+#Add shoreline points-------------------------------------------------------------
+shorepoints <- readOGR("../GIS","Shoreline_Points_GCS")
+names(shorepoints)
+names(points)
+proj4string(shorepoints)
+proj4string(points)
+
+# match column names between shore points and data points
+shorepoints <- shorepoints[,c('Lat','Lon','Bot_Ele','Id')]
+newnames <- c('Lat','Lon','Bot_Ele','ID')
+names(shorepoints) <- newnames
+
+#add shore depth and combine shoreline to points
+shorepoints$lakedepth <- 0
+allpoints <- spRbind(points, shorepoints)
+
+# reproject to meters
+utmproj <- '+proj=utm +zone=16 +ellps=GRS80 +datum=NAD83 +units=m +no_defs' 
+allpoints <- spTransform(allpoints, CRS(utmproj))
+
+# write out Shapefile
+writeOGR(allpoints,"../GIS","All_PointsWithShoreline", driver= "ESRI Shapefile")
+
+
+
+# variogram -----------------------------------------------------------------------
+# try it on full dataset
+vario <- variogram(lakedepth ~ 1, allpoints)
+plot(vario, col='black', main="Omnidirectional Variogram for All Data")
+
+
+# Randomly select points
+
