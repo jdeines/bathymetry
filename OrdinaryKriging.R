c6ca238964b7bf1b7a89d2d5d76108d46d72888f
cotterk <cotterk@msu.edu>
2014-12-09 17:31:31 -0500

    changed to raster

--- OrdinaryKriging.R
+++ OrdinaryKriging.R
@@ -28,25 +28,14 @@ everypoint <- points
 yes <- random5000$ID
 modelpoints <- points[!points$ID %in% yes, ] #points without the random 5000 points
 
-#Transforms to Normal Scores-------------------------------------------
 
-#modified function from AS #nscore_examples.R from Nov 18th lecture
-#same file has function to go back
-#NOTE: our's is not sorted and his was
-nscore <- function(x) {
-  # Takes a vector of values x and calculates their normal scores. Returns 
-  # a list with the scores and an ordered table of original values and
-  # scores, which is useful as a back-transform table. See backtr().
-  nscore <- qqnorm(x, plot.it = FALSE)$x  # normal score 
-  trn.table <- data.frame(x=x,nscore=nscore)
-  
-  return (list(nscore=nscore, trn.table=trn.table))
-}
+
+#Transforms to Normal Scores-------------------------------------------
 
 modelpoints.ns <- nscore(modelpoints$lakedepth)   # found normal score
 modelpoints.ns.df <- modelpoints.ns[[2]]                 # extract normal score data frame (back-transform table)
 Normalpoints <- cbind(as.data.frame(modelpoints),modelpoints.ns.df)   # combine nscores with lake depths/coordintes
-coordinates(Normalpoints) <- ~coords.x1+coords.x2             # re-spatialize
+coordinates(Normalpoints) <- ~x+y             # re-spatialize
 utmproj <- '+proj=utm +zone=16 +ellps=GRS80 +datum=NAD83 +units=m +no_defs' #give it the right projection
 proj4string(Normalpoints) <- utmproj
 
@@ -67,10 +56,11 @@ save(ok.krige, file=paste0(Rdatadirectory, 'ok.krige16.Rdata'))
 
 #change to raster
 okRaster <- raster(ok.krige[,'var1.pred'])
+spplot(okRaster, col.regions=rev(lakePal(200)), main= "Ordinary Kriging")
 
 #save raster
-Rdatadirectory <- 'S:/Projects/2013/Higgins_Lake/Higgins_Bath/KrigingProject/Higgins_Code/RData_Objects/'
-save(okRaster, file=paste0(Rdatadirectory,'okRaster.RData'))
+RdataOutputs <- 'S:/Projects/2013/Higgins_Lake/Higgins_Bath/KrigingProject/Higgins_Code/RData_Objects/Outputs/'
+save(okRaster, file=paste0(RdataOutputs,'okRaster.Rdata'))
 
 #Simulation Krige
 load(paste0(Rdatadirectory, 'Grid10m.RData'))
@@ -87,7 +77,7 @@ ok.sim.sp <- grid5m
 #Create an average map for simulation results
 ok.sim.sp$avg <- rowMeans(ok.sim, dims=1)
 spplot(ok.sim.sp, "avg", sp.layout = list(pts,border,jth), col.regions=ca.cols(20),
-             main=paste(me,"Joshua Tree habitat from simulation average", sep=" - "))
+             main="simulation average")
 
 # Make the model output spatial
 #h.sims.sp <- ca.grid
